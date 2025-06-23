3076a67167ff59039edc9688bd70f49dcb70ccdb
Azeem Jutt <inocentjutt02@gmail.com>
2025-06-23 13:07:25 -0700

    main.ts update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3001);
-
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
+  console.log(`✅ Server started on port ${port}`);
 }
 bootstrap();
